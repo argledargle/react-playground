259f9d5254ab95499e9f44760ae6c9f20ae0b2f8
Alex Peter <apyells@gmail.com>
2019-06-11 11:37:38 -0500

    Lots of updates

---
+++ src/lang-context/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppLang from './AppLang';
+import './index.css';
+
+ReactDOM.render(<AppLang />, document.getElementById('root'));
